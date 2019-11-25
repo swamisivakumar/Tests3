873f5f40c3bc9eeb3dc905041de686f9f6917a2c
swamisivakumar <swamisivakumar@live.com>
2019-11-25 16:56:28 +0530

    Done

--- WindowsFormsApp1/WindowsFormsApp1/Folder1/F2.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Folder1/F2.cs
@@ -8,9 +8,7 @@ namespace WindowsFormsApp1.Folder1
 {
     class F2
     {
-        int x1=1,x2=10;
-        int x22=123,x23=21;
-        int x11=121,x232=121;
+
         public F2()
         { 
             }
