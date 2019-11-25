33176a7187c6b7f781d574da699ffc2dfdb58f1b
swamisivakumar <swamisivakumar@live.com>
2019-11-25 17:17:26 +0530

    New Values added

--- WindowsFormsApp1/WindowsFormsApp1/Folder1/F1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Folder1/F1.cs
@@ -8,5 +8,14 @@ namespace WindowsFormsApp1.Folder1
 {
     class F1
     {
+
+      public F1 ()
+	{
+
+	}
+
+        public int Add { get; set; }
+        public int sub { get; set; }
+        public int Multiople { get; set;}
     }
 }
